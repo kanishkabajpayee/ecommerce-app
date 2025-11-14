ab3956c374efe1801ec7805b5067359a994305a8
kanishkabajpayee <kanishkabajpayee@gmail.com>
2025-11-14 17:56:30 +0530

    Login page and minor fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         <div className='flex gap-6'>
             <img onClick={(e)=>setShowSearch(!showSearch)} src ={assets.search_icon} className='w-5 cursor-pointer' />
             <div className='group relative'>
-                    <img src ={assets.profile_icon} className='w-5 cursor-pointer' />
+                    <Link to="/login"><img src ={assets.profile_icon} className='w-5 cursor-pointer' /></Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 text-gray-500'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
